85e5a9a54865aa3cf3734a231b5a3c00844624f3
unknown <melissikiss@gmail.com>
2022-06-13 20:24:59 -0700

    created desktop hero

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../homepage.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom';
+import Fade from 'react-reveal/Fade';
 
-import rainDropImg from '../assets/images/water-drops.jpg'
+// import rainDropImg from '../assets/images/water-drops.jpg'
 
 function Homepage() {
     return (
@@ -12,28 +13,33 @@ function Homepage() {
             </div> */}
             <div class="bg-img">
                 <div class="container">
-                <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">DryZone</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span> {/*CHANGE ICON LATER*/}
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ">
-                        <a class="nav-link" aria-current="page" href="/">Home</a>
-                        <a class="nav-link" href="/about">About</a>
-                        <a class="nav-link" href="/services">Services</a>
-                        <a class="nav-link" href="/contact">Contact</a>
+                    <nav class="navbar navbar-expand-lg">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="/">DryZone</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span> {/*CHANGE ICON LATER*/}
+                            </button>
+                            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                                <div class="navbar-nav ">
+                                    <a class="nav-link" aria-current="page" href="/">Home</a>
+                                    <a class="nav-link" href="/about">About</a>
+                                    <a class="nav-link" href="/services">Services</a>
+                                    <a class="nav-link" href="/contact">Contact</a>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <Fade top>
+                    <div className="container2">
+                        <p className="hero-title">{"WATERPROOFING &"}</p>
+                        <p className="hero-title">{"CONCRETE COATINGS LLC"}</p>
+                        <div className="hero-btns">
+                            <button className="hero-btn hbtn1"><Link to="/contact">Contact</Link></button>
+                            <button className="hero-btn hbtn2"><Link to="/about">About</Link></button>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </nav>
-                </div>
-
-                <div className="container2"> 
-                    <p className="hero-title">{"WATERPROOFING &"}</p>
-                    <p className="hero-title">{"CONCRETE COATINGS"}</p>
-                </div>
+                </Fade>
             </div>
         </main>
     );
