f5bb2bbcabdefa416290feb36dbfaa9c58f9deeb
unknown <melissikiss@gmail.com>
2022-06-13 18:24:37 -0700

    added hero image and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../navbar.css'
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">DryZone</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span> {/*CHANGE ICON LATER*/}
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ">
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                        <a class="nav-link" href="/about">About</a>
+                        <a class="nav-link" href="/services">Services</a>
+                        <a class="nav-link" href="/contact">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar; 
