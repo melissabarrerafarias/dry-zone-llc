ec5e5ad4a131df62d514881822c4c34959519170
unknown <melissikiss@gmail.com>
2022-08-01 11:59:55 -0700

    added about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,28 +2,53 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 
 import Navbar from '../components/Navbar';
-import silverio from '../assets/images/silverio.jpeg'
 import '../about.css';
 function About() {
     return (
         <div id="about">
             <Navbar />
             <section id="abt-dzone">
-                <Fade left>
-                    <h1>About Dry Zone</h1>
-                </Fade>
-                <div className="row">
-                    <div className="col-md-6 owner-img">
-                        <img src={silverio}/>
-                    </div>
-
-                    <div className="col-md-6">
-
-                    </div>
+                <div className="about-hero">
+                    <Fade top>
+                        <div className="abt-hero-title">
+                            <p>ABOUT DRY ZONE LLC</p>
+                        </div>
+                    </Fade>
                 </div>
 
-            </section>
-        </div>
+                <div id="about-paragraphs">
+                    {/* <h1>Origins</h1>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p> */}
+
+                    <h1>Experience</h1>
+                    <p>Silverio Barrera kicked off his construccion career in California, 1998 when he was just 23 years
+                        old. He worked in the roofing industry for two years until he switched over to his now specialty,
+                        waterproofing. He operated at a start-up company that grew into an award-winning business that
+                        used the newest products and up-to-date practices. For 22 years Silverio played a vital role
+                        working as an installer/technician, purchasing agent, safety supervisor, field supervisor, and
+                        senior supervisor and completed various freelance jobs on his own time. Every project being unique and
+                        presenting its own challenges, Silverio overcame them with innovative solutions. Now living in
+                        the Olympia, Washington region and confident in his abilities to provide quality results, he
+                        began his own company known as Dry Zone LLC. </p>
+
+                    <h1>Mission</h1>
+                    <p>Big or small jobs, Dry Zone LLC promises to deliver lasting quality results with particular attention to detail.
+                        Our goal is to make sure you're happy and satisfied with our services. We strive for excellence and pride
+                        ourselves in our work!  </p>
+
+
+                </div>
+
+            </section >
+        </div >
 
     )
 }
